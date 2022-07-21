237a53e0f6c9510a3afebae4bfde0923c1f7abdd
Jhonatan Macazana <46355455+jhonatanmacazana@users.noreply.github.com>
2022-07-21 00:48:14 -0500

    feat: add spinner and optimistic update

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -5,6 +5,8 @@ import { trpc } from "@/utils/trpc";
 
 import { Card } from "./Card";
 import { InputNumber, InputText } from "./Inputs";
+import { Transaction } from "@prisma/client";
+import { Spinner } from "./Spinner";
 
 export const Details: React.FC<{
   categoryId?: SummaryCategory;
@@ -15,23 +17,53 @@ export const Details: React.FC<{
 
   const tctx = trpc.useContext();
 
-  const { mutate: createTransactionMutation } = trpc.proxy.transaction.create.useMutation({
-    onSuccess: () => {
-      tctx.queryClient.invalidateQueries(["transaction.getByType"]);
-      tctx.queryClient.invalidateQueries(["transaction.summary"]);
-    },
-  });
   const transactions = trpc.proxy.transaction.getByType.useQuery(
     { type: categoryId! },
     { enabled: Boolean(categoryId) }
   );
 
-  const addTransaction = () => {
-    createTransactionMutation({ amount, description, type: categoryId! });
+  const { mutate: createTransactionMutation, isLoading } =
+    trpc.proxy.transaction.create.useMutation({
+      onSuccess: async (newTransaction) => {
+        if (!categoryId) return;
 
-    setAmount(0);
-    setDescription("");
-  };
+        setAmount(0);
+        setDescription("");
+
+        await Promise.all([
+          tctx.queryClient.cancelQueries(["transaction.getByType"]),
+          tctx.queryClient.cancelQueries(["transaction.summary"]),
+        ]);
+
+        const previousSummary = tctx.queryClient.getQueryData(["transaction.summary", null]);
+        const previousTransactions = tctx.queryClient.getQueryData([
+          "transaction.getByType",
+          { type: categoryId },
+        ]);
+
+        tctx.queryClient.setQueryData(["transaction.summary", null], (old: any) => ({
+          ...old,
+          [categoryId]: old[categoryId] + newTransaction.amount,
+        }));
+        tctx.queryClient.setQueryData(["transaction.getByType", { type: categoryId }], (old) => [
+          ...(old as Transaction[]),
+          newTransaction,
+        ]);
+
+        return { previousSummary, previousTransactions };
+      },
+      onError: (_err, _newTodo, context: any) => {
+        tctx.queryClient.setQueryData(["transaction.summary", null], context.previousSummary);
+        tctx.queryClient.setQueryData(
+          ["transaction.getByType", { type: categoryId }],
+          context.previousTransactions
+        );
+      },
+      onSettled: () => {
+        tctx.queryClient.invalidateQueries(["transaction.summary", null]);
+        tctx.queryClient.invalidateQueries(["transaction.getByType", { type: categoryId }]);
+      },
+    });
 
   const title = categoryLabel ? `Detalles - ${categoryLabel}` : "Detalles";
 
@@ -61,18 +93,20 @@ export const Details: React.FC<{
             />
 
             <button
-              className="w-full rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:w-auto"
-              onClick={() => addTransaction()}
+              className="flex w-56 items-center justify-center rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 disabled:cursor-not-allowed disabled:shadow-none dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              disabled={isLoading}
+              onClick={() => createTransactionMutation({ amount, description, type: categoryId })}
             >
+              {isLoading && <Spinner />}
               Registrar
             </button>
           </div>
 
           <p className="pt-4 pb-2 text-gray-400">Movimientos</p>
 
-          <ul className="flex w-full flex-grow flex-col gap-4 rounded-xl border border-slate-300 px-4 py-4 shadow-xl ">
+          <ul className="scrollbar flex max-h-60 w-full flex-grow flex-col gap-4 overflow-y-scroll rounded-xl border border-slate-300 px-4 py-4 shadow-xl">
             {transactions.data?.map((transaction) => (
-              <li className="flex gap-2" key={transaction.id}>
+              <li className="flex min-w-[2rem] gap-2" key={transaction.id}>
                 <p>{transaction.description}</p>
                 <p>{transaction.amount}</p>
               </li>
