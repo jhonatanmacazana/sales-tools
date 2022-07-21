e90e43011014521501cb2c101c7851180cc8a902
Jhonatan Macazana <46355455+jhonatanmacazana@users.noreply.github.com>
2022-07-21 02:59:56 +0000

    feat: retrigger queries after creation

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -13,11 +13,23 @@ export const Details: React.FC<{
   const [description, setDescription] = useState("");
   const [amount, setAmount] = useState(10);
 
+  const tctx = trpc.useContext();
+
   const { mutate: createTransactionMutation } = trpc.proxy.transaction.create.useMutation();
   const transactions = trpc.proxy.transaction.getByType.useQuery(
     { type: categoryId! },
     { enabled: Boolean(categoryId) }
   );
+
+  const addTransaction = () => {
+    createTransactionMutation({ amount, description, type: categoryId });
+
+    tctx.queryClient.invalidateQueries("transaction.getByType");
+    tctx.queryClient.invalidateQueries("transaction.summary");
+
+    setAmount(0);
+    setDescription("");
+  };
 
   const title = categoryLabel ? `Detalles - ${categoryLabel}` : "Detalles";
 
@@ -48,7 +60,7 @@ export const Details: React.FC<{
 
             <button
               className="w-full rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 sm:w-auto"
-              onClick={() => createTransactionMutation({ amount, description, type: categoryId })}
+              onClick={() => addTransaction()}
             >
               Registrar
             </button>
